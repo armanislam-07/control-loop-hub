cde6a02cd2ccfc375dc44630708f6d77e6cc74a6
Arman Islam <armanislamusa@gmail.com>
2026-05-18 01:02:09 -0700

    initialize project skeleton for esp32 i2c

---
+++ DUT/main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_intr_alloc.h"
+
+void app_main(void)
+{
+    /*
+     *
+     * isr when get i2c new information we do the motors
+     *
+     */
+    
+
+}
