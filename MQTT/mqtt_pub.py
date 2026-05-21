27a828ca4fe99d40ad38acf2acc6a146f86e31c5
Arman Islam <armanislamusa@gmail.com>
2026-05-21 16:35:30 -0700

    Add scaling for percentages in PWM

--- MQTT/mqtt_pub.py
+++ MQTT/mqtt_pub.py
@@ -1,4 +1,5 @@
 import paho.mqtt.publish as mqtt
 
 def publishMQTT(message):
+    message = int(int(message) * 10) # Scaling for percentages in PWM (Prescaler 1023) 
     mqtt.single("dutycycle", str(message), hostname="192.168.4.152")
